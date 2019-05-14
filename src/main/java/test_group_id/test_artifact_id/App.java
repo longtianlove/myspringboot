3b6d57392b4ba7e7e33998af02f32a89b0faf3be
longtianlove <longtianlove>
2019-05-14 10:17:20 +0800

    springboot

---
+++ src/main/java/test_group_id/test_artifact_id/App.java
@@ -0,0 +1,13 @@
+package test_group_id.test_artifact_id;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
